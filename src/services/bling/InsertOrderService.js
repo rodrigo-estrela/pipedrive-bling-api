0172088fd0409d74ba4e18ee1cb61b81383e5831
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-01-12 16:06:31 -0300

    added bling_post update logic after posting to bling

--- src/services/bling/InsertOrderService.js
+++ src/services/bling/InsertOrderService.js
@@ -5,22 +5,47 @@ const CreateDealXMLService = require('./CreateDealXMLService');
 
 const Deal = mongoose.model('deals');
 
+async function postOrderToBling(xml) {
+  const config = {
+    'Content-Type': 'application/xml',
+    params: {
+      xml,
+    },
+  };
+
+  const {
+    data: { retorno },
+  } = await bling.post('/pedido/json', null, config);
+
+  if (retorno && retorno.erros) return undefined;
+
+  return retorno;
+}
+
 class InsertOrderService {
   async execute() {
-    const dealsJSON = await Deal.find({ bling_posted: false });
-
     const createDealXML = new CreateDealXMLService();
 
-    const dealsXML = dealsJSON.map(async deal => {
-      const xml = createDealXML.execute(deal);
-      const config = {
-        'Content-Type': 'application/xml',
-        params: {
-          xml,
-        },
-      };
-      const response = await bling.post('/pedido', null, config);
-    });
+    const dealsJSON = await Deal.find({ bling_posted: false }).limit(100);
+
+    if (dealsJSON.length === 0) return;
+
+    const placedOrders = await Promise.all(
+      dealsJSON.map(async deal => {
+        const xml = createDealXML.execute(deal);
+
+        const response = await postOrderToBling(xml);
+
+        if (response) return deal.id;
+
+        return undefined;
+      }),
+    );
+
+    await Deal.updateMany(
+      { id: { $in: placedOrders } },
+      { bling_posted: true },
+    );
   }
 }
 
