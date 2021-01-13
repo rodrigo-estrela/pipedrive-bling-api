d951a82778717d8216108e5ae6be683e7f2014de
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-01-13 13:51:48 -0300

    improved error handling

--- src/modules/integration-api/services/pipedrive/GetWonDealsService.js
+++ src/modules/integration-api/services/pipedrive/GetWonDealsService.js
@@ -6,38 +6,34 @@ const pipedrive = require('../../apis/pipedrive');
 
 class GetWonDealsService {
   async execute() {
-    try {
-      const deals = await Deal.find({});
-      const start = deals ? Math.max(...deals.map(deal => deal.id)) : 0;
+    const deals = await Deal.find({});
+    const start = deals ? Math.max(...deals.map(deal => deal.id)) : 0;
 
-      const config = {
-        params: {
-          start,
-          status: 'won',
-        },
-      };
+    const config = {
+      params: {
+        start,
+        status: 'won',
+      },
+    };
 
-      const response = await pipedrive.get('/deals', config);
+    const response = await pipedrive.get('/deals', config);
 
-      if (!response || !response.data || !response.data.data) {
-        console.info('No new won deals');
-        return;
-      }
-
-      const dealsToBeAdded = response.data.data.map(deal => ({
-        id: deal.id,
-        title: deal.title,
-        value: deal.value,
-        currency: deal.currency,
-        status: deal.status,
-        won_time: format(new Date(deal.won_time), 'yyy-MM-dd'),
-        org_name: deal.org_name,
-      }));
-
-      await Deal.insertMany(dealsToBeAdded);
-    } catch (err) {
-      console.error(err);
+    if (!response || !response.data || !response.data.data) {
+      console.info('No new won deals');
+      return;
     }
+
+    const dealsToBeAdded = response.data.data.map(deal => ({
+      id: deal.id,
+      title: deal.title,
+      value: deal.value,
+      currency: deal.currency,
+      status: deal.status,
+      won_time: format(new Date(deal.won_time), 'yyy-MM-dd'),
+      org_name: deal.org_name,
+    }));
+
+    await Deal.insertMany(dealsToBeAdded);
   }
 }
 
--- src/shared/database/index.js
+++ src/shared/database/index.js
@@ -5,7 +5,12 @@ const config = require('../../config');
 require('../../modules/integration-api/models/Deals');
 require('../../modules/integration-api/models/Sales');
 
-mongoose.connect(config.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch(error => {
+    console.error(error);
+    throw new Error('Unable to connect MongoDB');
+  });
